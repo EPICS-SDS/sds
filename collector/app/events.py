991fc43ba3c08d0379e2baa94778083efc6b12de
Ross Titmarsh <ross.titmarsh@ess.eu>
2022-04-27 17:25:18 +0200

    Removed HTTP trigger

--- collector/app/events.py
+++ collector/app/events.py
@@ -38,34 +38,6 @@ class SDSEvent:
 class DataOnDemand(SDSEvent):
     type = "dod"
 
-    async def trigger(self):
-        now = datetime.now()
-        context = Context('pva')
-
-        tasks = [next_value(context, pv, now) for pv in self.pvs]
-        result = await asyncio.gather(*tasks)
-        return result
-
 
 class PostMortem(SDSEvent):
     type = "postmortem"
-
-
-async def next_value(context, pv, now=datetime.now()):
-    future = asyncio.Future()
-
-    async def cb(value):
-        if isinstance(value, Exception):
-            future.set_exception(value)
-        # The monitor will first call back any currently set values, the test
-        # is needed to ignore them
-        elif value.timestamp > now.timestamp():
-            future.set_result(value)
-
-    sub = context.monitor(pv, cb)
-    # Wait for value from the callback
-    result = await future
-    # Close subscription once result is received
-    sub.close()
-    # Return the PV name and value
-    return pv, result
