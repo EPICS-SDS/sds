99609ba35a9c5c91d30f89283488333c63f57174
Ross Titmarsh <ross.titmarsh@ess.eu>
2022-06-10 14:32:27 +0200

    Added env vars and config to collector service

--- collector/app/config.py
+++ collector/app/config.py
@@ -1,9 +1,10 @@
-from pydantic import BaseSettings, DirectoryPath, FilePath
+from pydantic import BaseSettings, DirectoryPath, FilePath, AnyHttpUrl
 
 
 class Settings(BaseSettings):
     output_dir: DirectoryPath
     collector_definitions: FilePath
+    indexer_url: AnyHttpUrl
     collector_timeout: int = 2
 
 
