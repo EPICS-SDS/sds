8df2d71b784ab47db6ba87f33a1f60d61114b9ae
Ross Titmarsh <ross.titmarsh@ess.eu>
2022-06-22 12:19:17 +0200

    Changed model field type

--- common/common/models/collector.py
+++ common/common/models/collector.py
@@ -1,10 +1,10 @@
 from common.db.base_class import Base
-from common.db.fields import Date, Integer, Text
+from common.db.fields import Date, Integer, Keyword, Text
 
 
 class Collector(Base):
     name: Text
-    pvs: Text
+    pvs: Keyword
     event_name: Text
     event_code: Integer
     created: Date
