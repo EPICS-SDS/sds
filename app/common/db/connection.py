308c2bc91b128a321f00562fd01e09d51680c2f3
Juan F. Esteban Müller <JuanF.EstebanMuller@esss.se>
2022-08-09 14:09:46 +0200

    Silencing Warnings from transport loggers

--- app/common/db/connection.py
+++ app/common/db/connection.py
@@ -20,6 +20,11 @@ async def get_connection():
 
 
 async def wait_for_connection(timeout: int = settings.retry_connection):
+    # Silencing Warnings from transport loggers
+    transport_logger = logging.getLogger("elastic_transport")
+    transport_logger_level = transport_logger.level
+    transport_logger.setLevel("ERROR")
+
     async with get_connection() as connection:
         trials = 0
         while not await connection.ping():
@@ -29,6 +34,8 @@ async def wait_for_connection(timeout: int = settings.retry_connection):
 
             trials += 1
             await asyncio.sleep(1)
+
+    transport_logger.setLevel(transport_logger_level)
 
 
 def with_connection(func):
