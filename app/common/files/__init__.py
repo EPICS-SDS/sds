b20098937e83b09f5765a51b0d7fc2b4651268d7
Juan F. Esteban Müller <JuanF.EstebanMuller@ess.eu>
2022-09-27 11:45:39 +0200

    the nexusformat package enables compression in HDF5 by default. Disabling it to improve performance

--- app/common/files/__init__.py
+++ app/common/files/__init__.py
@@ -2,3 +2,8 @@ from .collector import Collector
 from .config import settings
 from .dataset import Dataset, DatasetSchema
 from .event import Event
+
+from nexusformat.nexus import nxsetcompression
+
+# Disabling HDF5 compression to improve performance
+nxsetcompression("None")
