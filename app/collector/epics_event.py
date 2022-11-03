539073518ba2f924d0f9ef3109252665e8338333
Juan F. Esteban Müller <JuanF.EstebanMuller@ess.eu>
2022-11-03 14:36:44 +0100

    fixing parsing the new PV structure

--- app/collector/epics_event.py
+++ app/collector/epics_event.py
@@ -22,24 +22,24 @@ class EpicsEvent(Event):
         )
 
         # pulse_id
-        pulse_id = value.get("cycleId")
+        pulse_id = value.raw.get("cycleId")
         if pulse_id is not None:
             # For the moment we use the same pulse ID for the triggering event, since we have no circular buffers
             values.update(pulse_id=pulse_id, trigger_pulse_id=pulse_id)
 
         # eventCode
-        sds_info = value.get("sdsInfo")
+        sds_info = value.raw.get("sdsInfo")
         if sds_info is not None:
             values.update(
                 trigger_date=datetime.fromtimestamp(
-                    sds_info.raw.timeStamp.secondsPastEpoch
-                    + sds_info.raw.timeStamp.nanoseconds * 1e-9
+                    sds_info.timeStamp.secondsPastEpoch
+                    + sds_info.timeStamp.nanoseconds * 1e-9
                 ),
             )
         # For the moment take the acqEvt code instead of the sds code (missing)
-        acq_evt = value.get("acqEvt")
+        acq_evt = value.raw.get("acqEvt")
         if acq_evt is not None:
             values.update(
-                timing_event_code=acq_evt.raw.code,
+                timing_event_code=acq_evt.code,
             )
         return values
