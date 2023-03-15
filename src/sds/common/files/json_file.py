275176c173642cd5e2262ac6060d8ccf99fef660
Juan F. Esteban Müller <JuanF.EstebanMuller@ess.eu>
2023-03-15 13:23:57 +0100

    adding attributes to the json structure

--- src/sds/common/files/json_file.py
+++ src/sds/common/files/json_file.py
@@ -36,24 +36,48 @@ class JsonFile:
         Write a combined Json file from a list of files (for retriever)
         """
         try:
-            json_dict = {}
+            json_dict = {"attrs": {}, "events": {}}
 
             for dataset in self.datasets.values():
                 origin = File(settings.storage_path / dataset.path, "r")
 
-                json_dict[f"sds_event_{dataset.sds_event_pulse_id}"] = {}
-                for pulse in origin["entry"][f"sds_event_{dataset.sds_event_pulse_id}"]:
-                    json_dict[f"sds_event_{dataset.sds_event_pulse_id}"][pulse] = {}
-                    for pv in origin["entry"][
-                        f"sds_event_{dataset.sds_event_pulse_id}"
-                    ][pulse]:
+                for att in origin["entry"].attrs:
+                    # Remove nexus tags
+                    if not att.startswith("NX"):
+                        json_dict["attrs"][att] = origin["entry"].attrs[att]
+
+                json_event = json_dict["events"][
+                    f"sds_event_{dataset.sds_event_pulse_id}"
+                ] = {"attrs": {}, "pulses": {}}
+                origin_event = origin["entry"][
+                    f"sds_event_{dataset.sds_event_pulse_id}"
+                ]
+                for att in origin_event.attrs:
+                    # Remove nexus tags
+                    if not att.startswith("NX"):
+                        json_event["attrs"][att] = origin_event.attrs[att]
+                for pulse in origin_event:
+                    origin_pulse = origin_event[pulse]
+                    json_pulse = json_event["pulses"][pulse] = {"attrs": {}, "pvs": {}}
+
+                    for att in origin_pulse.attrs:
+                        # Remove nexus tags
+                        if not att.startswith("NX"):
+                            json_pulse["attrs"][att] = origin_pulse.attrs[att]
+
+                    for pv in origin_pulse:
                         if self.pvs is None or pv in self.pvs:
-                            json_dict[f"sds_event_{dataset.sds_event_pulse_id}"][pulse][
-                                pv
-                            ] = self.todict(
-                                origin["entry"][
-                                    f"sds_event_{dataset.sds_event_pulse_id}"
-                                ][pulse][pv]
+                            json_pulse["pvs"][pv] = {"attrs": {}, "values": {}}
+
+                            for att in origin_pulse[pv].attrs:
+                                # Remove nexus tags
+                                if not att.startswith("NX"):
+                                    json_pulse["pvs"][pv]["attrs"][att] = origin_pulse[
+                                        pv
+                                    ].attrs[att]
+
+                            json_pulse["pvs"][pv]["values"] = self.todict(
+                                origin_pulse[pv]
                             )
 
             return json_dict
