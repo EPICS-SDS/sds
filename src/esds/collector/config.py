f3d50c86de0521e14d885dccef29f26c4672f696
Juan F. Esteban Müller <JuanF.EstebanMuller@ess.eu>
2025-01-20 22:20:59 +0100

    adding description to some of the configuration parameters of the collector service

--- src/esds/collector/config.py
+++ src/esds/collector/config.py
@@ -1,6 +1,6 @@
 import logging
 
-from pydantic import AnyHttpUrl, FilePath
+from pydantic import AnyHttpUrl, Field, FilePath
 from pydantic_settings import BaseSettings
 
 
@@ -8,10 +8,18 @@ class Settings(BaseSettings):
     log_level: int | str = logging.INFO
     collector_host: str = "0.0.0.0"
     collector_definitions: FilePath
-    flush_file_delay: float = 2
-    collector_timeout: float = 2
+    flush_file_delay: float = Field(
+        2, description="Maximum delay to flush data from a collector task to file."
+    )
+    collector_timeout: float = Field(
+        2,
+        description="Timeout for a collector task. It starts to count when the first event is received for a given SDS event ID.",
+    )
     http_connection_timeout: float = 5
-    events_per_file: int = 1
+    events_per_file: int = Field(
+        1,
+        description="Maximum number of SDS events to be stored in a single NeXus file. The file is kept open for more events until the collector tasks time out.",
+    )
     indexer_url: AnyHttpUrl
     wait_for_indexer: bool = True
     indexer_timeout_min: int = 1
